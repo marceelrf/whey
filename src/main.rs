19b730ec8d65c1da13459a0a03494e777a1463b1
delta.gembio <marcel.ferreira@unesp.br>
2025-12-02 15:44:08 -0300

    clap args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let _matches = App::new("whey")
+        .version("0.1.0")
+        .author("Marcel Ferreira <marcel.ferreira@unesp.br>")
+        .about("whey: protein analysis")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1)
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false)
+        )
+        .get_matches();
+
+    println!("{:#?}", _matches)
 }
